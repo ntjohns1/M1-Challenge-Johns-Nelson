5745d7089bd0646f7df5476c2b8b1ef80d4e2ec6
ntjohns1 <nelsontjohns@gmail.com>
2022-02-15 21:52:09 -0500

    completed exercise 2 calculator object

---
+++ CalculatorObject/src/main/java/CalculatorApplication.java
@@ -0,0 +1,26 @@
+public class CalculatorApplication {
+    public static void main(String[] args) {
+//                - 1 + 1
+//                - 23 - 52
+//                - 34 * 2
+//                - 12 / 3
+//                - 12 / 7
+//                - 3.4 + 2.3
+//                - 6.7 * 4.4
+//                - 5.5 - 0.5
+//                - 10.8 / 2.2
+
+        Calculator calc = new Calculator();
+
+        System.out.println("1 + 1 = " + calc.add(1,1 ));
+        System.out.println("23 - 52 = " + calc.subtract(23, 52));
+        System.out.println("34 * 2 = " + calc.multiply(34,2));
+        System.out.println("12 / 3 = " + calc.divide(12,3));
+        System.out.println("12 / 7 = " + calc.divide(12,7));
+        System.out.println("3.4 + 2.3 = " + calc.add(3.4, 2.3));
+        System.out.println("6.7 * 4.4 = " + calc.multiply(6.7, 4.4));
+        System.out.println("5.5 - 0.5 = " + calc.subtract(5.5,.5));
+        System.out.println("10.8 / 2.2 = " + calc.divide(10.8,2.2));
+
+    }
+}
---
+++ CalculatorObject/src/test/java/CalculatorTest.java
@@ -0,0 +1,106 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CalculatorTest {
+
+    Calculator calc;
+
+    @org.junit.Before
+    public void setUp() throws Exception {
+       calc = new Calculator();
+    }
+
+    @Test
+    public void shouldReturnSumOfTwoInts() {
+
+        String failMessage = "Expected calculator to handle integer addition.";
+
+        assertEquals(failMessage, 2, calc.add(1,1));
+        assertEquals(failMessage, 101, calc.add(156,-55));
+        assertEquals(failMessage, -33, calc.add(-56,23));
+        assertEquals(failMessage, 34, calc.add(20,14));
+    }
+
+    @Test
+    public void shouldReturnDifferenceOfTwoInts() {
+
+        String failMessage = "Expected calculator to handle integer subtraction.";
+
+        assertEquals(failMessage, 0, calc.subtract(1,1));
+        assertEquals(failMessage, -3, calc.subtract(1,4));
+        assertEquals(failMessage, 56, calc.subtract(76,20));
+        assertEquals(failMessage, -7, calc.subtract(-3,4));
+
+    }
+
+    @Test
+    public void shouldReturnProductOfTwoInts() {
+
+        String failMessage = "Expected calculator to handle integer multiplication.";
+
+        assertEquals(failMessage, 16, calc.multiply(4,4));
+        assertEquals(failMessage, 5, calc.multiply(5,1));
+        assertEquals(failMessage, 0, calc.multiply(41,0));
+        assertEquals(failMessage, -16, calc.multiply(-4,4));
+
+    }
+
+    @Test
+    public void shouldReturnQuotientOfTwoInts() {
+
+        String failMessage = "Expected calculator to handle integer division.";
+
+        assertEquals(failMessage, 2, calc.divide(12,6));
+        assertEquals(failMessage, -17, calc.divide(-51,3));
+        assertEquals(failMessage, 0, calc.divide(0,2));
+        assertEquals(failMessage, 9, calc.divide(19,2));
+
+    }
+
+    @Test
+    public void shouldReturnSumOfTwoDoubles() {
+
+        String failMessage = "Expected calculator to handle double addition.";
+
+        assertEquals(failMessage, 7.6, calc.add(1.1,6.5), .01);
+        assertEquals(failMessage, 224.4, calc.add(156.5,67.9), .01);
+        assertEquals(failMessage, -1.1, calc.add(-6.7,5.6), .01);
+        assertEquals(failMessage, 6.62, calc.add(4.55,2.07), .001);
+    }
+
+    @Test
+    public void shouldReturnDifferenceOfTwoDoubles() {
+
+        String failMessage = "Expected calculator to handle double subtraction.";
+
+        assertEquals(failMessage, 0, calc.subtract(1.1,1.1), .01);
+        assertEquals(failMessage, 468.5, calc.subtract(545.3,76.8), .01);
+        assertEquals(failMessage, -8.3, calc.subtract(-5.6,2.7), .01);
+        assertEquals(failMessage, -12.8, calc.subtract(-4.33,8.47), .001);
+
+    }
+
+    @Test
+    public void shouldReturnProductOfTwoDoubles() {
+
+        String failMessage = "Expected calculator to handle double multiplication.";
+
+        assertEquals(failMessage, 40.96, calc.multiply(6.4,6.4), .01);
+        assertEquals(failMessage, 105.64, calc.multiply(55.6,1.9), .01);
+        assertEquals(failMessage, 0, calc.multiply(4.1,0), .01);
+        assertEquals(failMessage, -26.0988, calc.multiply(-5.46,4.78), .001);
+    }
+
+    @Test
+    public void shouldReturnQuotientOfTwoDoubles() {
+
+        String failMessage = "Expected calculator to handle double division.";
+
+        assertEquals(failMessage, 7.1, calc.divide(42.6,6), .001);
+        assertEquals(failMessage, -1.457, calc.divide(-5.1,3.5), .001);
+        assertEquals(failMessage, 0, calc.divide(0,2.6), .001);
+        assertEquals(failMessage, 0.6898, calc.divide(1.98,2.87), .0001);
+
+    }
+}
