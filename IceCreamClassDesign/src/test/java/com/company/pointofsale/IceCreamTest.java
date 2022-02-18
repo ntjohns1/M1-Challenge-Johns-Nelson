e2e4940b6df30688c59849b2a7982379eee5c3c5
ntjohns1 <nelsontjohns@gmail.com>
2022-02-18 00:10:30 -0500

    finished testing for pointofsale.IceCream class

--- IceCreamClassDesign/src/test/java/com/company/pointofsale/IceCreamTest.java
+++ IceCreamClassDesign/src/test/java/com/company/pointofsale/IceCreamTest.java
@@ -13,18 +13,43 @@ public class IceCreamTest {
 
     @Before
     public void setUp() throws Exception {
-        vanilla = new com.company.pointofsale.IceCream("vanilla", .70);
-        chocolate = new com.company.pointofsale.IceCream("chocolate", .70);
-        strawberry = new com.company.pointofsale.IceCream("strawberry", .70);
+        vanilla = new com.company.pointofsale.IceCream("vanilla", 2.76);
+        chocolate = new com.company.pointofsale.IceCream("chocolate", 2.80);
+        strawberry = new com.company.pointofsale.IceCream("strawberry", 2.86);
     }
 
     // Tests for Point of Sale Class
+    @Test
     public void shouldAddIntValueToQuantity() {
+        vanilla.restock(12);
+        chocolate.restock(384);
+        strawberry.restock(64);
+
+        assertEquals(396, vanilla.getQuantity());
+        assertEquals(768, chocolate.getQuantity());
+        assertEquals(448, strawberry.getQuantity());
     }
 
+    @Test
     public void shouldDecrementQuantityAndWritePriceToConsole() {
+        vanilla.makeSale(1);
+        chocolate.makeSale(2);
+        strawberry.makeSale(3);
+
+        assertEquals(380, vanilla.getQuantity());
+        assertEquals(376, chocolate.getQuantity());
+        assertEquals(372, strawberry.getQuantity());
     }
 
+    @Test
     public void shouldInrementQuantityAndWriteMessageToConsole() {
+        vanilla.voidSale(1);
+        chocolate.voidSale(2);
+        strawberry.voidSale(3);
+
+        assertEquals(388, vanilla.getQuantity());
+        assertEquals(392, chocolate.getQuantity());
+        assertEquals(396, strawberry.getQuantity());
+
     }
 }
